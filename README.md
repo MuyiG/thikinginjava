13b87674df49fb70107cd249ed81115101b24d0b
sunshine <yangguang01@weidian.com>
2017-01-29 00:49:06 +0800

    添加LinkedListFeatures

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # thikinginjava
-《Thinking in Java》一书中的实验性代码，主要涉及到一些比较基础而又容易误解的知识，记下来备忘。
+主要是《Thinking in Java》一书中的内容，也会涉及到一些别的比较基础而又容易误解的知识，记下来备忘。
 
 ## 章节 : Package 对应关系如下：
 * Initialization & Cleanup : init  
---
+++ src/com/sunshinevvv/thinkinginjava/containers/LinkedListFeatures.java
@@ -0,0 +1,36 @@
+package com.sunshinevvv.thinkinginjava.containers;
+
+import java.util.Collections;
+import java.util.LinkedList;
+
+/**
+ * Created by sunshine on 2017/1/29.
+ */
+public class LinkedListFeatures {
+    public static void main(String[] args) {
+        LinkedList<String> pets = new LinkedList<>();
+        Collections.addAll(pets, "Rat", "Manx", "Cymric", "Mutt", "Pug", "Cymric", "Pug");
+        System.out.println("pets: " + pets);
+
+        // 这三个方法基本相同，区别在于getFirst和element在列表为空时会抛异常，peek则会返回null
+        System.out.println("getFirst() : " + pets.getFirst());
+        System.out.println("element() : " + pets.element());
+        System.out.println("peek() : " + pets.peek());
+        // 同上，也是poll特殊
+        System.out.println("remove() : " + pets.remove());
+        System.out.println("removeFirst() : " + pets.removeFirst());
+        System.out.println("poll() : " + pets.poll());
+
+        System.out.println("pets: " + pets);
+        pets.addFirst("Hamster1");
+        System.out.println("After addFirst: " + pets);
+
+        // 这三个方法一样
+        pets.offer("Hamster2");
+        System.out.println("After offer: " + pets);
+        pets.addLast("Hamster3");
+        System.out.println("After addLast: " + pets);
+        pets.add("Hamster4");
+        System.out.println("After add: " + pets);
+    }
+}
