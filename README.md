aa849344f30ecdaa07ab90ce77655d977f178648
sunshine <yangguang01@weidian.com>
2017-02-07 15:02:03 +0800

    添加RunnableDemo

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # thikinginjava
-主要是《Thinking in Java》一书中的内容，也会涉及到一些别的比较基础而又容易误解的知识，记下来备忘。
+主要是《Thinking in Java》一书中的内容，也会涉及到一些在别处遇到的比较基础而又容易误解的知识，记下来备忘。
 
 ## 章节 : Package 对应关系如下：
 * Initialization & Cleanup : init  
@@ -8,4 +8,4 @@
 * Interfaces : interfaces
 * Inner Classes : innerclass
 * Holding Your Objects : containers
-
+* Concurrency : concurrency
---
+++ src/com/sunshinevvv/thinkinginjava/concurrency/RunnableDemo.java
@@ -0,0 +1,32 @@
+package com.sunshinevvv.thinkinginjava.concurrency;
+
+/**
+ * 基本的Thread和Runnable用法
+ */
+public class RunnableDemo implements Runnable {
+
+    private String name;
+
+    public RunnableDemo(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + " " + name + " running.");
+        // 啥也不干，sleep1秒
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(Thread.currentThread().getName() + " " + name + " exiting.");
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            Thread thread = new Thread(new RunnableDemo("Runnable-" + i), "Thread-" + i);
+            thread.start();
+        }
+    }
+}
