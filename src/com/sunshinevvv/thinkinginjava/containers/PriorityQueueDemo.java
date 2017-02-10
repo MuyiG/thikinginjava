e211a66c898a711df4d1777c8adcaf04cff72d30
Sunshinevvv <sunshine.vvv@163.com>
2017-02-11 00:20:18 +0800

    添加优先队列

---
+++ src/com/sunshinevvv/thinkinginjava/containers/PriorityQueueDemo.java
@@ -0,0 +1,56 @@
+package com.sunshinevvv.thinkinginjava.containers;
+
+import java.util.*;
+
+/**
+ * Created by 光 on 2017/2/10.
+ *
+ * 优先队列的示例
+ */
+public class PriorityQueueDemo {
+
+    private static void printQueue(Queue queue) {
+        while (!queue.isEmpty()) {
+            System.out.print(queue.poll() + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+        Random random = new Random(47);
+        for (int i = 0; i < 10; i++) {
+            priorityQueue.add(random.nextInt(i + 10));
+        }
+        printQueue(priorityQueue);
+
+        List<Integer> ints = Arrays.asList(25, 22, 20,
+                18, 14, 9, 3, 1, 1, 2, 3, 9, 14, 18, 21, 23, 25);
+        priorityQueue = new PriorityQueue<Integer>(ints);
+        printQueue(priorityQueue);
+        priorityQueue = new PriorityQueue<Integer>(
+                ints.size(), Collections.reverseOrder());
+        priorityQueue.addAll(ints);
+        printQueue(priorityQueue);
+
+        // Queue允许重复
+        String fact = "EDUCATION SHOULD ESCHEW OBFUSCATION";
+        List<String> strings = Arrays.asList(fact.split(""));
+        PriorityQueue<String> stringPQ =
+                new PriorityQueue<String>(strings);
+        printQueue(stringPQ);
+        stringPQ = new PriorityQueue<String>(
+                strings.size(), Collections.reverseOrder());
+        stringPQ.addAll(strings);
+        printQueue(stringPQ);
+
+        // 一种消除重复的手段是提前用Set处理
+        Set<Character> charSet = new HashSet<Character>();
+        for(char c : fact.toCharArray())
+            charSet.add(c); // Autoboxing
+        PriorityQueue<Character> characterPQ =
+                new PriorityQueue<Character>(charSet);
+        printQueue(characterPQ);
+    }
+
+}
