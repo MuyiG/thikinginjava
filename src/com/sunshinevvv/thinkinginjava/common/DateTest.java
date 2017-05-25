2a3f02e6c1d3698ef7e2941b509d6c383160dc4d
杨光 <yangguang01@weidian.com>
2017-05-25 11:34:36 +0800

    DateTest

---
+++ src/com/sunshinevvv/thinkinginjava/common/DateTest.java
@@ -0,0 +1,12 @@
+package com.sunshinevvv.thinkinginjava.common;
+
+import java.util.Date;
+
+/**
+ * Created by sunshine on 2017/5/25.
+ */
+public class DateTest {
+    public static void main(String[] args) {
+        System.out.println(new Date().getTime());
+    }
+}
