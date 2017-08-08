ec6cd3451a0f0e961b9ff08c05fba747f3419712
Sunshinevvv <sunshine.vvv@163.com>
2017-08-09 00:15:02 +0800

    ..

--- src/com/sunshinevvv/thinkinginjava/concurrency/share/EvenGenerator.java
+++ src/com/sunshinevvv/thinkinginjava/concurrency/share/EvenGenerator.java
@@ -4,7 +4,8 @@ public class EvenGenerator extends IntGenerator {
     private int currentEvenValue = 0;
 
     public int next() {
-        ++currentEvenValue; // Danger point here! 因为++不是原子操作？
+        ++currentEvenValue; // Danger point here!
+        // 多线程执行时，可能会出现这样的情况：A线程只执行了第一个++，还没来得及执行完第二个，此时切换到另一个线程B执行两个++，此时线程B就会返回一个奇数。
         ++currentEvenValue;
         return currentEvenValue;
     }
