08096e552578179d9313c86c399d1e9872f103a6
Sunshinevvv <sunshine.vvv@163.com>
2017-09-02 00:34:01 +0800

    termination

---
+++ src/com/sunshinevvv/thinkinginjava/concurrency/termination/CloseResource.java
@@ -0,0 +1,31 @@
+package com.sunshinevvv.thinkinginjava.concurrency.termination;
+
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 本机测试显示，关闭System.in后，还需要输入一个回车才会终止程序，why？
+ */
+public class CloseResource {
+    public static void main(String[] args) throws Exception {
+        ExecutorService exec = Executors.newCachedThreadPool();
+        ServerSocket server = new ServerSocket(8080);
+        InputStream socketInput =
+                new Socket("localhost", 8080).getInputStream();
+        exec.execute(new IOBlocked(socketInput));
+        exec.execute(new IOBlocked(System.in));
+        TimeUnit.MILLISECONDS.sleep(100);
+        System.out.println("Shutting down all threads");
+        exec.shutdownNow();
+        TimeUnit.SECONDS.sleep(1);
+        System.out.println("Closing " + socketInput.getClass().getName());
+        socketInput.close(); // Releases blocked thread
+        TimeUnit.SECONDS.sleep(1);
+        System.out.println("Closing " + System.in.getClass().getName());
+        System.in.close(); // Releases blocked thread
+    }
+}
