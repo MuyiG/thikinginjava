2f794c53424d1fab24a294c13b7d0dd0de712773
杨光 <yangguang01@weidian.com>
2017-09-08 12:33:08 +0800

    LongTest

--- src/com/sunshinevvv/thinkinginjava/common/BigIntegerTest.java
+++ src/com/sunshinevvv/thinkinginjava/common/BigIntegerTest.java
@@ -1,5 +1,6 @@
 package com.sunshinevvv.thinkinginjava.common;
 
+import java.math.BigDecimal;
 import java.math.BigInteger;
 
 /**
@@ -9,5 +10,9 @@ public class BigIntegerTest {
     public static void main(String[] args) {
         BigInteger bigInteger = new BigInteger("355115018671761029363", 10); // radix=10可选，因为默认就是10进制
         System.out.println(bigInteger);
+
+        BigDecimal b1 = new BigDecimal("14.9");
+        System.out.println(b1.multiply(new BigDecimal("100")).longValue());
+        System.out.println(b1.longValue() * 100); // 精度丢失
     }
 }
--- src/com/sunshinevvv/thinkinginjava/common/LongTest.java
+++ src/com/sunshinevvv/thinkinginjava/common/LongTest.java
@@ -1,10 +1,16 @@
 package com.sunshinevvv.thinkinginjava.common;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by sunshine on 2017/4/25.
  */
 public class LongTest {
     public static void main(String[] args) {
-        Long l = Long.parseLong("998119023928672548840");
+//        Long l = Long.parseLong("998119023928672548840");
+
+        Map<String, Object> emptyMap = new HashMap<>();
+        long l1 = (Long)emptyMap.get(""); // NPE
     }
 }
