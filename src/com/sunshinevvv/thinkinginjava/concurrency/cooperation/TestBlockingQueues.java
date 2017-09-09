8be6c6e29552870954554a612b0321bcd59074e9
Sunshinevvv <sunshine.vvv@163.com>
2017-09-09 15:21:40 +0800

    cooperation

---
+++ src/com/sunshinevvv/thinkinginjava/concurrency/cooperation/TestBlockingQueues.java
@@ -0,0 +1,62 @@
+package com.sunshinevvv.thinkinginjava.concurrency.cooperation;
+
+import com.sunshinevvv.thinkinginjava.concurrency.basic.LiftOff;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.SynchronousQueue;
+
+/**
+ * 既是生产者(add)，又是消费者(run)
+ */
+class LiftOffRunner implements Runnable {
+    private BlockingQueue<LiftOff> rockets;
+
+    public LiftOffRunner(BlockingQueue<LiftOff> queue) {
+        rockets = queue;
+    }
+
+    public void add(LiftOff lo) {
+        try {
+            rockets.put(lo);
+        } catch (InterruptedException e) {
+            System.out.println("Interrupted during put()");
+        }
+    }
+
+    public void run() {
+        try {
+            while (!Thread.interrupted()) {
+                LiftOff rocket = rockets.take();
+                rocket.run(); // Use this thread，就像普通方法调用，没有起新线程
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Waking from take()");
+        }
+        System.out.println("Exiting LiftOffRunner");
+    }
+}
+
+public class TestBlockingQueues {
+    static void test(String msg, BlockingQueue<LiftOff> queue) {
+        System.out.println(msg);
+        LiftOffRunner runner = new LiftOffRunner(queue);
+        Thread t = new Thread(runner);
+        t.start();
+        for (int i = 0; i < 5; i++) {
+            runner.add(new LiftOff());
+        }
+        t.interrupt();
+        System.out.println("Finished " + msg + " test");
+    }
+
+    public static void main(String[] args) {
+        test("LinkedBlockingQueue", // Unlimited size
+                new LinkedBlockingQueue<LiftOff>());
+        test("ArrayBlockingQueue", // Fixed size
+                new ArrayBlockingQueue<LiftOff>(3));
+        test("SynchronousQueue", // Size of 1
+                new SynchronousQueue<LiftOff>());
+    }
+}
