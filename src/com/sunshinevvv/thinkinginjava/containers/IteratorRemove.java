e5cca18e9b0b014a391cb14b45e4f38adb425161
杨光 <yangguang01@weidian.com>
2018-01-02 10:32:48 +0800

    test

--- src/com/sunshinevvv/thinkinginjava/containers/IteratorRemove.java
+++ src/com/sunshinevvv/thinkinginjava/containers/IteratorRemove.java
@@ -10,8 +10,16 @@ import java.util.List;
 public class IteratorRemove {
     public static void main(String[] args) {
         List<Integer> integerList = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+        // ConcurrentModificationException
+//        for (Integer i : integerList) {
+//            if (i == 3) {
+//                integerList.remove(i);
+//            }
+//        }
+
+        // 不会抛错，因为碰巧5是最后一个元素
         for (Integer i : integerList) {
-            if (i == 3) {
+            if (i == 5) {
                 integerList.remove(i);
             }
         }
--- src/com/sunshinevvv/thinkinginjava/strings/UrlEncodeTest.java
+++ src/com/sunshinevvv/thinkinginjava/strings/UrlEncodeTest.java
@@ -1,12 +1,15 @@
 package com.sunshinevvv.thinkinginjava.strings;
 
 import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 import java.net.URLEncoder;
 
 public class UrlEncodeTest {
     public static void main(String[] args) {
         try {
             System.out.println(URLEncoder.encode("杨光:)", "UTF-8"));
+            System.out.println(URLEncoder.encode("b oo", "UTF-8").replace("+", "%20"));
+            System.out.println(URLDecoder.decode("b+oo", "UTF-8"));
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
