cecc65399569723003490db50d46ee289ea5548b
杨光 <sunshine.vvv@163.com>
2018-03-06 01:01:56 +0800

    init

--- src/com/sunshinevvv/thinkinginjava/init/InitOrder.java
+++ src/com/sunshinevvv/thinkinginjava/init/InitOrder.java
@@ -1,20 +1,22 @@
 package com.sunshinevvv.thinkinginjava.init;
 
 /**
- * Created by 光 on 2016/8/22.
+ * 类初始化的顺序是先clinit再init。
+ * clinit包含静态变量 + 静态初始化块，
+ * init包含变量 + 初始化块 + 构造函数。
  */
 public class InitOrder {
-
+    // 静态变量
     public static String staticField = "静态变量";
-
-    // 变量
-    public String field = "变量";
 
     // 静态初始化块
     static {
-        System.out.println(staticField);
+        System.out.println(staticField); // 如果这句话放在staticField定义之前会报错：illegal forward reference.
         System.out.println("静态初始化块");
     }
+
+    // 变量
+    public String field = "变量";
 
     // 初始化块
     {
