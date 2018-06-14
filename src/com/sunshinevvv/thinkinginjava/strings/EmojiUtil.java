db0bcb5d22b665b50df094a46425d59de6bf8a1e
杨光 <yangguang01@weidian.com>
2018-06-14 20:48:39 +0800

    String

--- src/com/sunshinevvv/thinkinginjava/strings/EmojiUtil.java
+++ src/com/sunshinevvv/thinkinginjava/strings/EmojiUtil.java
@@ -13,6 +13,7 @@ public class EmojiUtil {
     public static void main(String[] args) {
         System.out.println(filterPrivateCodePoint("直率的性子\uE022\uE022\uE022"));
         System.out.println(filterPrivateCodePoint("直率的性子😝"));
+        System.out.println(filterPrivateCodePoint("直率的性子"));
     }
 
     public static String filterPrivateCodePoint(String source) {
