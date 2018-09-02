68f5081a45cd5702e91c089a99e3c5afdf34f5b4
杨光 <yangguang01@weidian.com>
2018-09-03 00:38:05 +0800

    VariantTest

--- src/com/sunshinevvv/thinkinginjava/generics/EraseTest.java
+++ src/com/sunshinevvv/thinkinginjava/generics/EraseTest.java
@@ -5,6 +5,9 @@ import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 关于泛型类型擦除的一些实验
+ */
 public class EraseTest {
     public static void main(String[] args) {
         List<String> strList = new ArrayList<>();
@@ -13,8 +16,9 @@ public class EraseTest {
         intList.add(1);
         Class<?> strListClass = strList.getClass();
         Class<?> intListClass = intList.getClass();
-        System.out.println("List<String> class:" + strListClass + ", element class:" + strList.get(0).getClass());
-        System.out.println("List<Integer> class:" + intListClass + ", element class:" + intList.get(0).getClass());
+        System.out.println("List<String> class:" + strListClass);
+        System.out.println("List<Integer> class:" + intListClass);
+        System.out.println("Class equals? " + strListClass.equals(intListClass));
 
         System.out.println("===");
         Erasure<String> erasure = new Erasure<>("hello");
@@ -34,15 +38,9 @@ public class EraseTest {
         for (Method method : erasure1.getClass().getDeclaredMethods()) {
             System.out.println("Method :" + method);
         }
-        erasure1.setObject("haha"); // 成功绕过编译期检查
+        erasure1.setObject("haha"); // 使用原始类型成功绕过编译期检查，但是不推荐该做法
         String s = (String) erasure1.getObject();
         System.out.println(s);
-
-        Erasure[] erasures = new Erasure[10]; // OK
-//        Erasure<String>[] erasures2 = new Erasure<String>[10]; // compile error
-        Erasure<String>[] erasures3 = (Erasure<String>[]) new Erasure[10]; // work around
-        erasures3[0] = new Erasure<>("");
-//        erasures3[0] = new Erasure<>(1); // compile error
     }
 }
 
--- src/com/sunshinevvv/thinkinginjava/generics/VariantTest.java
+++ src/com/sunshinevvv/thinkinginjava/generics/VariantTest.java
@@ -3,9 +3,14 @@ package com.sunshinevvv.thinkinginjava.generics;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 我的理解：泛型不支持协变是一种设计上的决策。
+ * Java编译器可以选择支持泛型协变，但是一旦在编译期放过去了，运行期就再也无法区分了，所以没有选择支持；
+ * 数组由于有完善的运行期检查，因此选择了编译期支持协变，运行时抛错的方式。
+ */
 public class VariantTest {
     public static void main(String[] args) {
-        // Java数组是协变的，但是运行时会抛错
+        // Java数组是协变的，但是如果试图插入非法的类型时会抛RuntimeException
         Object[] objects = new Integer[10];
         objects[0] = 1;
 //        objects[1] = "str"; // java.lang.ArrayStoreException: java.lang.String
@@ -13,12 +18,9 @@ public class VariantTest {
         // Java泛型不是协变的，把抛错提前到了编译期
 //        List<Object> objectList = new ArrayList<Integer>(); // Compile Error
 
-        List<String>[] strList = (List<String>[]) new ArrayList[10];
-        strList[0] = new ArrayList<>();
-        List[] list = strList;
-        list[0] = new ArrayList<Integer>();
-        list[0].add(1);
-        list[0].add(1.1);
-        list[0].add(" ");
+        // 但可以使用通配符和上下界实现类似的效果
+        List<?> list1 = new ArrayList<Integer>();
+        List<? extends Number> list2 = new ArrayList<Integer>();
+        List<? super Integer> list3 = new ArrayList<Number>();
     }
 }
