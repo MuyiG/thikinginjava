b0ed963717d6ccbcc855aa665cb66f2f0e3f42d7
杨光 <yangguang01@weidian.com>
2018-09-03 16:38:14 +0800

    JDKProxyTest备注

--- src/com/sunshinevvv/thinkinginjava/reflection/JDKProxyTest.java
+++ src/com/sunshinevvv/thinkinginjava/reflection/JDKProxyTest.java
@@ -29,6 +29,10 @@ class MyInvocationHandler implements InvocationHandler {
     }
 }
 
+/**
+ * 配置了VM参数：-Dsun.misc.ProxyGenerator.saveGeneratedFiles=true
+ * 会把动态生成的代理类字节码存放在项目根目录下：$Proxy0
+ */
 public class JDKProxyTest {
     public static void main(String[] args) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
         // 这里有两种写法，我们采用略微复杂的一种写法，这样更有助于大家理解。
