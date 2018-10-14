bf109b3e92402bdffd7a3e1c4c6f31271a4ba4e9
杨光 <yangguang01@weidian.com>
2018-10-15 00:09:23 +0800

    注释

--- src/com/sunshinevvv/thinkinginjava/network/SimpleServer.java
+++ src/com/sunshinevvv/thinkinginjava/network/SimpleServer.java
@@ -10,14 +10,15 @@ public class SimpleServer {
         try {
             ServerSocket ss = new ServerSocket(8888);
             System.out.println("Server start....");
-            // 收消息
+            // 建立连接
             Socket s = ss.accept();
             System.out.println("Client:" + s.getInetAddress().getLocalHost() + " connected");
 
-            // 发消息
+            // 收消息
             BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
             String mess = br.readLine();
             System.out.println("Client message：" + mess);
+            // 发消息
             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(s.getOutputStream()));
             bw.write("Hello, I am Server.\n");
             bw.flush();
