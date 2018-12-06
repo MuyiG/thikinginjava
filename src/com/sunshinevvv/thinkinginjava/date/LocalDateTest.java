d070da4414d4b649470ea5fc9ddc6772fe264d67
杨光 <yangguang01@weidian.com>
2018-12-06 21:37:29 +0800

    ThreadLocalRandom

--- src/com/sunshinevvv/thinkinginjava/date/LocalDateTest.java
+++ src/com/sunshinevvv/thinkinginjava/date/LocalDateTest.java
@@ -3,14 +3,6 @@ package com.sunshinevvv.thinkinginjava.date;
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 
-/**
- * <p>文件描述：</p>
- * <p>版权所有： 版权所有(C)2011-2099</p>
- * <p>公   司： 口袋购物</p>
- * <p>其他说明：</p>
- * <p>作   者：杨光</p>
- * <p>完成日期：2018/12/6</p>
- */
 public class LocalDateTest {
     public static void main(String[] args) {
         testUntil();
