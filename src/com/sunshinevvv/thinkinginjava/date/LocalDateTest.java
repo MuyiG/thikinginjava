14c13e5f30f9711e26d7a3743592ef82291d6c99
杨光 <yangguang01@weidian.com>
2018-12-14 16:41:31 +0800

    +1

--- src/com/sunshinevvv/thinkinginjava/date/LocalDateTest.java
+++ src/com/sunshinevvv/thinkinginjava/date/LocalDateTest.java
@@ -2,6 +2,7 @@ package com.sunshinevvv.thinkinginjava.date;
 
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
+import java.util.Calendar;
 
 public class LocalDateTest {
     public static void main(String[] args) {
@@ -14,5 +15,10 @@ public class LocalDateTest {
         System.out.println(start.until(start, ChronoUnit.DAYS));
         System.out.println(start.until(end, ChronoUnit.DAYS));
 
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(1544772832000L);
+        LocalDate localDate = LocalDate.of(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH) + 1, calendar.get(Calendar.DAY_OF_MONTH));
+        LocalDate now = LocalDate.now();
+        System.out.println(localDate.until(now, ChronoUnit.DAYS));
     }
 }
