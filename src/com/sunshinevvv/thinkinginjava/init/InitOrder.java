cc27c58eda93f6370ebc0e5051073b59f3a95dcd
杨光 <yangguang01@weidian.com>
2019-07-18 10:58:52 +0800

    InitOrder

--- src/com/sunshinevvv/thinkinginjava/init/InitOrder.java
+++ src/com/sunshinevvv/thinkinginjava/init/InitOrder.java
@@ -6,19 +6,19 @@ package com.sunshinevvv.thinkinginjava.init;
  * init包含变量 + 初始化块 + 构造函数。
  */
 public class InitOrder {
-    // 静态变量
+    // 静态变量 initializer for static filed
     public static String staticField = "静态变量";
 
-    // 静态初始化块
+    // 静态初始化块 static initializer
     static {
         System.out.println(staticField); // 如果这句话放在staticField定义之前会报错：illegal forward reference.
         System.out.println("静态初始化块");
     }
 
-    // 变量
+    // 变量 initializer for non-static filed
     public String field = "变量";
 
-    // 初始化块
+    // 初始化块 initializer
     {
         System.out.println(field);
         System.out.println("初始化块");
@@ -29,7 +29,14 @@ public class InitOrder {
         System.out.println("构造器");
     }
 
+    public static void staticMethod() {
+        System.out.println("StaticMethod");
+    }
+
     public static void main(String[] args) {
+        // 静态方法的调用只会触发静态初始化
+//        staticMethod();
+        // 实例化会触发所有的初始化
         new InitOrder();
     }
 
