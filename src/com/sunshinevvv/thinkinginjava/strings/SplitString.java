6b90eb36812925a7e8fb94b2ee27592917e0b0f2
Gabriel Yang <gabriel.yang@zoom.us>
2023-03-31 18:36:29 +0800

    SplitString

--- src/com/sunshinevvv/thinkinginjava/strings/SplitString.java
+++ src/com/sunshinevvv/thinkinginjava/strings/SplitString.java
@@ -5,9 +5,47 @@ package com.sunshinevvv.thinkinginjava.strings;
  */
 public class SplitString {
     public static void main(String[] args) {
-        String s1 = "13141";
-        String s2 = "1231~342342";
-        System.out.println(s1.split("~")[0]);
-        System.out.println(s2.split("~")[0]);
+        new SplitString().testPerformance();
+    }
+
+    public void testPerformance() {
+        int lineNum = 1000000;
+        String largeString = buildLargeString(lineNum);
+        System.out.println("line number: " + lineNum);
+
+        long start, end;
+        String firstLine;
+
+        start = System.currentTimeMillis();
+        String[] arr = largeString.split("\n");
+        firstLine = arr[0];
+        firstLine.contains("non_existing1");
+        firstLine.contains("non_existing2");
+        firstLine.contains("non_existing3");
+        end = System.currentTimeMillis();
+        System.out.println("split + contains*3 time cost (ms): " + (end - start));
+
+        start = System.currentTimeMillis();
+        largeString.contains("non_existing1");
+        largeString.contains("non_existing2");
+        largeString.contains("non_existing3");
+        end = System.currentTimeMillis();
+        System.out.println("directly contains*3 time cost (ms): " + (end - start));
+
+        start = System.currentTimeMillis();
+        firstLine = largeString.substring(0, largeString.indexOf("\n"));
+        firstLine.contains("non_existing1");
+        firstLine.contains("non_existing2");
+        firstLine.contains("non_existing3");
+        end = System.currentTimeMillis();
+        System.out.println("substring + contains*3 time cost (ms): " + (end - start));
+    }
+
+    private String buildLargeString(int num) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < num; i++) {
+            stringBuilder.append("test_email" + i + "@zoom.us\n");
+        }
+        return stringBuilder.toString();
     }
 }
